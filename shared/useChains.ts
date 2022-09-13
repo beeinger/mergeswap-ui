cf41a8175b9805fe382e9b3e6a1848ce6572fad4
beeinger <sulecki.maciek@gmail.com>
2022-09-13 17:08:54 +0100

    chainId cleanup

--- shared/useChains.ts
+++ shared/useChains.ts
@@ -1,101 +1,20 @@
-import { useEffect, useState } from "react";
-
-import { NativeCurrency } from "./types";
 import { createContext } from "react";
 import { useEthers } from "@usedapp/core";
 
-const handleChainChange = async (
-  chainId: string,
-  chainName: string,
-  rpcUrls: string[],
-  nativeCurrency: NativeCurrency
-) => {
-  try {
-    if (typeof window === "undefined" || !window.ethereum) {
-      // TODO: UI modal instead (?)
-      alert("Please install MetaMask and reload the page.");
-      throw new Error("ethereum not found");
-    }
-
-    await window.ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [{ chainId }],
-    });
-  } catch (switchError) {
-    // This error code indicates that the chain has not been added to MetaMask.
-    if (switchError.code === 4902) {
-      try {
-        await window.ethereum.request({
-          method: "wallet_addEthereumChain",
-          params: [
-            {
-              chainId,
-              chainName,
-              nativeCurrency,
-              rpcUrls,
-            },
-          ],
-        });
-      } catch (addError) {
-        console.error(addError);
-      }
-    }
-    console.error(switchError);
-  }
-};
-
-export const switchToPoS = async () =>
-    handleChainChange(
-      "0x13881",
-      "Mumbai", // PoS
-      ["https://matic-mumbai.chainstacklabs.com"],
-      {
-        name: "MATIC",
-        symbol: "MATIC",
-        decimals: 18,
-      }
-    ),
-  switchToPoW = async () =>
-    handleChainChange(
-      "0x5",
-      "Goerli Test Network", // PoW
-      ["https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
-      {
-        name: "ETH",
-        symbol: "ETH",
-        decimals: 18,
-      }
-    );
-
 export const ChainsContext = createContext<ReturnType<typeof useChains>>(null);
 
+const POS_CHAIN_ID = 80001,
+  POW_CHAIN_ID = 5;
+
 export default function useChains() {
-  const { activateBrowserWallet } = useEthers();
-  const [chainId, setChainId] = useState(undefined);
-
-  useEffect(() => {
-    const injectedChainId = parseInt(window.ethereum.networkVersion);
-    setChainId(injectedChainId);
-
-    // Handling MetaMask event.
-    window.ethereum.on("networkChanged", (_chainId: string) => {
-      if (parseInt(_chainId) !== injectedChainId) {
-        console.log("Network change detected, reloading app...");
-        window.location.reload();
-      }
-    });
-
-    return () => {
-      window.ethereum.removeAllListeners("networkChanged");
-    };
-  }, []);
+  const { activateBrowserWallet, chainId, switchNetwork } = useEthers();
 
   const handleSwitchToPoS = async () => {
-      await switchToPoS();
+      switchNetwork(POS_CHAIN_ID);
       activateBrowserWallet();
     },
     handleSwitchToPoW = async () => {
-      await switchToPoW();
+      switchNetwork(POW_CHAIN_ID);
       activateBrowserWallet();
     };
 
@@ -104,8 +23,8 @@ export default function useChains() {
     handleSwitchToPoS,
     handleSwitchToPoW,
     // TODO: update with production values.
-    isPoS: chainId === 80001,
-    isPoW: chainId === 5,
-    isETHAtAll: chainId === 80001 || chainId === 5,
+    isPoS: chainId === POS_CHAIN_ID,
+    isPoW: chainId === POW_CHAIN_ID,
+    isETHAtAll: chainId === POS_CHAIN_ID || chainId === POW_CHAIN_ID,
   };
 }
