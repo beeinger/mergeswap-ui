6a2700983dfb6a768316762dbfaaad9955ecc3bd
Aimeric Seguin <aimeric.seguin@gmail.com>
2022-09-14 22:09:28 +0400

    WIP mint

--- components/PoWToPoS/PoWToPoS.tsx
+++ components/PoWToPoS/PoWToPoS.tsx
@@ -1,5 +1,11 @@
 import React, { useContext, useState, useEffect } from "react";
-import { useContractFunction, useEtherBalance, useEthers } from "@usedapp/core";
+import {
+  useCalls,
+  useContractFunction,
+  useEtherBalance,
+  useEthers,
+  useSendTransaction,
+} from "@usedapp/core";
 
 import { ChainsContext } from "shared/useChains";
 import { Contract } from "@ethersproject/contracts";
@@ -16,6 +22,8 @@ import {
   TransactionReceipt,
   TransactionResponse,
 } from "@ethersproject/abstract-provider";
+import { encodeProof } from "shared/utils/encode-proof";
+import { PoW } from "shared/chains/custom";
 
 export default function PoWToPoS() {
   const { isPoW, provider } = useContext(ChainsContext);
@@ -33,6 +41,14 @@ export default function PoWToPoS() {
       depositPowInterface,
       provider
     );
+
+  const wPowEthInterface = new Interface([
+      "function mint(uint256 depositId, address recipient, uint256 amount, uint256 depositBlockNumber, bytes memory storageProof)",
+      "function updateDepositContractStorageRoot(uint256 blockNumber, bytes memory accountProof)",
+    ]),
+    wPowEthAddress = process.env.NEXT_PUBLIC_WPOWETH_POS_ADDRESS,
+    wPowEthContract = new Contract(wPowEthAddress, wPowEthInterface, provider);
+
   const { state, send, resetState } = useContractFunction(
     depositPowContract,
     "deposit",
@@ -112,6 +128,89 @@ export default function PoWToPoS() {
     }
   }, [state, provider, depositPowContract.interface]);
 
+  const { sendTransaction } = useSendTransaction();
+
+  const handleMint = async () => {
+    const depositEntities = [
+      {
+        id: "0x0",
+        amount: "10000000000000000",
+        depositor: "0x6b477781b0e68031109f21887e6b5afeaaeb002b",
+        recipient: "0x6b477781b0e68031109f21887e6b5afeaaeb002b",
+        blockNumber: "7590815",
+        storageProof: null,
+      },
+      // {
+      //   id: "0x1",
+      //   amount: "20000000000000000",
+      //   depositor: "0x6b477781b0e68031109f21887e6b5afeaaeb002b",
+      //   recipient: "0x6b477781b0e68031109f21887e6b5afeaaeb002b",
+      //   blockNumber: "7590865",
+      //   storageProof: null,
+      // },
+    ];
+    console.log("->", process.env.NEXT_PUBLIC_DEPOSIT_POW_ADDRESS);
+    // Get proofs
+    for (const depositEntity of depositEntities) {
+      const paddedSlot = hexZeroPad("0x3", 32);
+      const paddedKey = hexZeroPad(depositEntity.id, 32);
+      const itemSlot = keccak256(paddedKey + paddedSlot.slice(2));
+      const storageAt = await PoW.provider.getStorageAt(
+        process.env.NEXT_PUBLIC_DEPOSIT_POW_ADDRESS,
+        itemSlot
+      );
+      console.log("storageAt", storageAt);
+
+      const proof = await PoW.provider.send("eth_getProof", [
+        process.env.NEXT_PUBLIC_DEPOSIT_POW_ADDRESS,
+        [storageAt],
+        "latest",
+      ]);
+      console.log("proof", proof);
+      depositEntity.storageProof = encodeProof(proof.storageProof[0].proof);
+      // TODO: check if needed
+      const accountProofEncoded = encodeProof(proof.accountProof);
+      const update = await sendTransaction({
+        to: process.env.NEXT_PUBLIC_WPOWETH_POS_ADDRESS,
+        data: wPowEthContract.interface.encodeFunctionData(
+          "updateDepositContractStorageRoot",
+          [depositEntity.blockNumber, accountProofEncoded]
+        ),
+      });
+      console.log("update", update);
+    }
+    console.table(depositEntities);
+
+    const calldata =
+      depositEntities.map(
+        ({ id, amount, recipient, blockNumber, storageProof }) =>
+          wPowEthContract.interface.encodeFunctionData("mint", [
+            parseInt(id, 16).toString(),
+            recipient,
+            amount,
+            blockNumber,
+            storageProof,
+          ])
+      ) ?? [];
+
+    const r = await sendTransaction({
+      to: process.env.NEXT_PUBLIC_WPOWETH_POS_ADDRESS,
+      data: calldata.join(""),
+    });
+    console.log(r);
+    // const results = useCalls(calls) ?? [];
+    // results.forEach((result, idx) => {
+    //   if (result && result.error) {
+    //     console.error(
+    //       `Error encountered calling 'mint' on ${calls[idx]?.contract.address}: ${result.error.message}`
+    //     );
+    //   }
+    // });
+    // console.log("results", results);
+    // if (depositEntities.length) setDepositEntities([]);
+    // return results.map((result) => result?.value?.[0]);
+  };
+
   return isPoW ? (
     //? Always active (or when someone has any ETH on PoW)
     <div>
@@ -140,12 +239,12 @@ export default function PoWToPoS() {
     //? Should be active only when someone has sent ETH PoW to PoS
     <div>
       <h4>Mint ETH PoW you&apos;ve sent to PoS</h4>
-      <input
+      {/* <input
         placeholder="ETH PoW amount"
         onChange={(e) => setPoWEthAmount(e.target.value)}
         value={poWEthAmount}
-      />
-      <button>mint</button>
+      /> */}
+      <button onClick={handleMint}>mint</button>
     </div>
   );
 }
